ce45283e5b6b9b4c932e9ebf69800c11774404e5
Rayhan Al Kavey <91812273+RayhanalKavey@users.noreply.github.com>
2025-02-18 21:21:15 +0600

    Add filter based on selected options and also change the selected button style.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const transactions = ref([]);
 const add = ref(false);
+const selectedFilter = ref("ALL");
 
 onMounted(() => {
   const storedTransactions =
@@ -18,16 +19,21 @@ const deleteTransaction = (id) => {
   );
   localStorage.setItem("transactions", JSON.stringify(transactions.value));
 };
+
+// Computed property to filter transactions based on the selected filter
+const filteredTransactions = computed(() => {
+  if (selectedFilter.value === "INCOME") {
+    return transactions.value.filter((t) => t.type === "Income");
+  } else if (selectedFilter.value === "EXPENSE") {
+    return transactions.value.filter((t) => t.type === "Expense");
+  }
+  return transactions.value;
+});
 </script>
 
 <template>
   <div class="container mx-auto p-6">
     <h1 class="text-center text-6xl font-bold mb-6">Expense Tracker</h1>
-    <button
-      @click="add = true"
-      class="bg-blue-500 text-white px-4 py-2 rounded-md">
-      New Transaction
-    </button>
 
     <!-- Modal -->
     <div
@@ -45,9 +51,47 @@ const deleteTransaction = (id) => {
         <add-transaction v-model="transactions" @close="add = false" />
       </div>
     </div>
+    <!-- Filter Buttons -->
 
+    <div class="flex justify-between mt-4">
+      <div class="flex gap-2">
+        <button
+          @click="selectedFilter = 'ALL'"
+          :class="[
+            'px-4 py-2 rounded',
+            selectedFilter === 'ALL' ? 'bg-blue-500 text-white' : 'bg-gray-200',
+          ]">
+          All Transactions
+        </button>
+        <button
+          @click="selectedFilter = 'INCOME'"
+          :class="[
+            'px-4 py-2 rounded',
+            selectedFilter === 'INCOME'
+              ? 'bg-green-500 text-white'
+              : 'bg-gray-200',
+          ]">
+          Incomes
+        </button>
+        <button
+          @click="selectedFilter = 'EXPENSE'"
+          :class="[
+            'px-4 py-2 rounded',
+            selectedFilter === 'EXPENSE'
+              ? 'bg-red-500 text-white'
+              : 'bg-gray-200',
+          ]">
+          Expenses
+        </button>
+      </div>
+      <button
+        @click="add = true"
+        class="bg-blue-500 text-white px-4 py-2 rounded-md">
+        New Transaction
+      </button>
+    </div>
     <transaction-list
-      :transactions="transactions"
+      :transactions="filteredTransactions"
       @delete-transaction="deleteTransaction" />
   </div>
 </template>
